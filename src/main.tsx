81d936c6f16c1692e5fcd38c231cfa7d696593c9
Neeraj Kapil <47920662+neerajkapil33@users.noreply.github.com>
2026-09-01 23:04:16 +0530

    Add React application bootstrap

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode><App /></StrictMode>,
+);
